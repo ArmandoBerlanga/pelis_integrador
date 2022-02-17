04cccb490ef602ec5e7c7ebd5aca146ee6f87b94
Armando Berlanga <Jose.berlangam@udem.edu>
2022-02-16 22:46:02 -0600

    update scripts e intento de pagination

--- backend/doc/DB/01 Scripts.sql
+++ backend/doc/DB/01 Scripts.sql
@@ -40,19 +40,220 @@
 
 -- 3) SPS
 
-    -- EXEC dbo.CargarListaPeliculas 
-    ALTER PROCEDURE dbo.CargarListaPeliculas 
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+
+    CREATE PROCEDURE [dbo].[AgregarPeliculaProtagonista] 
+        @PeliculaID INT = NULL,
+        @NombreProtagonista VARCHAR(100) = NULL,
+        @ProtagonistaID INT = NULL
+    AS 
+    BEGIN
+
+        SELECT TOP (1) @ProtagonistaID = ProtagonistaID 
+        FROM Protagonista 
+        WHERE LOWER(NombreProtagonista) = LOWER(TRIM(@NombreProtagonista))
+
+        IF (@ProtagonistaID IS NULL)
+        BEGIN
+            INSERT INTO Protagonista VALUES (@NombreProtagonista)
+            SELECT TOP (1) @ProtagonistaID = ProtagonistaID FROM Protagonista ORDER BY ProtagonistaID DESC
+        END
+
+        INSERT INTO PeliculaProtagonista VALUES (@PeliculaID, @ProtagonistaID)
+
+        SELECT *
+        FROM Protagonista 
+        WHERE ProtagonistaID = @ProtagonistaID
+        
+    END
+    GO
+
+    --
+    --
+    --
+
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+
+    ALTER PROCEDURE [dbo].[BorrarPelicula] 
+        @PeliculaID INT = NULL
     AS 
     BEGIN
 
         SELECT P.*, C.DescripcionCorta, C.DescripcionLarga, D.NombreDirector, PP.Poster
         FROM Pelicula P 
             INNER JOIN Categoria C ON P.CategoriaID = C.CategoriaID
-            INNER JOIN Director D ON D.DirectorID = P.DirectorID
+            LEFT JOIN Director D ON D.DirectorID = P.DirectorID
             LEFT JOIN PosterPelicula PP ON P.PeliculaID = PP.PeliculaID 
+        WHERE P.PeliculaID = ISNULL(@PeliculaID, P.PeliculaID)
+
+        DELETE FROM PeliculaProtagonista
+        WHERE PeliculaID = @PeliculaID
+
+        DELETE FROM PosterPelicula
+        WHERE PeliculaID = @PeliculaID
+
+        DELETE FROM Pelicula
+        WHERE PeliculaID = @PeliculaID
 
     END
+    GO
 
     --
     --
     --
+
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+    -- EXEC dbo.CargarListaActores
+    -- EXEC dbo.CargarListaActores @PeliculaID=17
+    CREATE PROCEDURE [dbo].[CargarListaActores] 
+        @PeliculaID INT = NULL
+    AS 
+    BEGIN
+
+        SELECT P.ProtagonistaID, P.NombreProtagonista
+        FROM PeliculaProtagonista PP 
+            INNER JOIN Protagonista P ON PP.ProtagonistaID =    P.ProtagonistaID
+        WHERE PP.PeliculaID = ISNULL(@PeliculaID, PP.PeliculaID)
+
+    END
+    GO
+
+    --
+    --
+    --
+
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+    -- EXEC dbo.CargarListaPeliculas 
+    ALTER PROCEDURE [dbo].[CargarListaPeliculas] 
+        @PeliculaID INT = NULL
+    AS 
+    BEGIN
+
+        SELECT P.*, C.DescripcionCorta, C.DescripcionLarga, D.NombreDirector, PP.Poster
+        FROM Pelicula P 
+            INNER JOIN Categoria C ON P.CategoriaID = C.CategoriaID
+            LEFT JOIN Director D ON D.DirectorID = P.DirectorID
+            LEFT JOIN PosterPelicula PP ON P.PeliculaID = PP.PeliculaID 
+        WHERE P.PeliculaID = ISNULL(@PeliculaID, P.PeliculaID)
+
+    END
+    GO
+
+    --
+    --
+    --
+
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+    -- EXEC dbo.CargarProtagonistas @PeliculaID=1 
+    CREATE PROCEDURE [dbo].[CargarProtagonistas]
+    -- DECLARE
+        @PeliculaID INT = NULL
+    AS 
+    BEGIN
+
+        SELECT P.ProtagonistaID 'ProtagonistaID', P.NombreProtagonista 'NombreProtagonista'
+        FROM PeliculaProtagonista PP INNER JOIN Protagonista P ON PP.ProtagonistaID = P.ProtagonistaID
+        WHERE PP.PeliculaID = @PeliculaID
+
+    END
+    GO
+
+    --
+    --
+    --
+
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+    -- EXEC dbo.DeletePeliculaProtagonista @PeliculaID=15, @ProtagonistaID=4
+
+    -- INSERT INTO PeliculaProtagonista VALUES (15,4), (15,3)
+
+    CREATE PROCEDURE [dbo].[DeletePeliculaProtagonista] 
+        @PeliculaID INT = NULL,
+        @ProtagonistaID INT = NULL
+    AS 
+    BEGIN
+
+        SELECT P.ProtagonistaID, P.NombreProtagonista
+        FROM PeliculaProtagonista PP INNER JOIN Protagonista P ON PP.ProtagonistaID = P.ProtagonistaID
+        WHERE PeliculaID = @PeliculaID AND P.ProtagonistaID = @ProtagonistaID
+
+        DELETE FROM PeliculaProtagonista
+        WHERE PeliculaID = @PeliculaID AND ProtagonistaID = @ProtagonistaID
+
+    END
+    GO
+
+    --
+    --
+    --
+
+    SET ANSI_NULLS ON
+    GO
+    SET QUOTED_IDENTIFIER ON
+    GO
+    -- EXEC dbo.CargarListaPeliculas 
+    CREATE PROCEDURE [dbo].[GuardarPelicula](
+        @PeliculaID INT = 0,
+        @NombrePelicula VARCHAR(100),
+        @Duracion DECIMAL(5, 2) = NULL,
+        @CategoriaID INT = NULL,
+        @DirectorID INT = NULL,
+        @Poster VARCHAR(MAX) = NULL
+    )AS 
+    BEGIN
+
+        IF(@PeliculaID = 0)
+        BEGIN
+            INSERT INTO dbo.Pelicula VALUES (@NombrePelicula, @Duracion, @CategoriaID, @DirectorID)
+            SELECT TOP (1) @PeliculaID = P.PeliculaID FROM dbo.Pelicula P ORDER BY P.PeliculaID DESC
+        END
+
+        ELSE
+        BEGIN
+            UPDATE dbo.Pelicula
+            SET 
+                NombrePelicula = ISNULL(@NombrePelicula, NombrePelicula),
+                Duracion = ISNULL(@Duracion, Duracion),
+                CategoriaID = ISNULL(@CategoriaID, CategoriaID),
+                DirectorID = ISNULL(@DirectorID, DirectorID)
+            WHERE PeliculaID = @PeliculaID
+        END
+
+        IF(@Poster IS NOT NULL)
+        BEGIN
+            IF (@PeliculaID NOT IN (SELECT P.PeliculaID FROM dbo.PosterPelicula P))
+                INSERT INTO dbo.PosterPelicula VALUES (@PeliculaID, @Poster)
+            ELSE
+                UPDATE dbo.PosterPelicula 
+                SET Poster = @Poster
+                WHERE PeliculaID = @PeliculaID
+        END
+
+        -- return de pelicula
+        SELECT P.*, C.DescripcionCorta, C.DescripcionLarga, D.NombreDirector, PP.Poster
+        FROM Pelicula P 
+            INNER JOIN Categoria C ON P.CategoriaID = C.CategoriaID
+            LEFT JOIN Director D ON D.DirectorID = P.DirectorID
+            LEFT JOIN PosterPelicula PP ON P.PeliculaID = PP.PeliculaID 
+        WHERE P.PeliculaID = ISNULL(@PeliculaID, P.PeliculaID)
+
+    END
+    GO
--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,20 +1,15 @@
 <template>
 <q-page>
 
-    <div class="peliculas-container">
-        <div class="titulo-wrapper">
-            <p class="titulo">Listado <span class="marca">Check<span>Flix</span></span></p>
-            <p class="subtitulo">CheckFlix es una aplicación para checar películas y series</p>
-        </div>
+    <div class="titulo-wrapper">
+        <p class="titulo">Listado <span class="marca">Check<span>Flix</span></span></p>
+        <p class="subtitulo">CheckFlix es una aplicación para checar películas y series</p>
+        <!-- <q-pagination class="pagination" v-model="state.current" :max="5" /> -->
+    </div>
 
+    <div class="peliculas-container">
         <div v-for="(peli, index) in pelisFiltradas" :key="index">
-            <Pelicula 
-                :id="peli.peliculaId"
-                :title="peli.nombrePelicula" :duration="peli.duracion" 
-                :categorie="peli.descripcionCorta" 
-                :director="peli.nombreDirector == null ? '' : peli.nombreDirector" 
-                :poster="peli.poster == null ? '' : peli.poster" 
-            />
+            <Pelicula :id="peli.peliculaId" :title="peli.nombrePelicula" :duration="peli.duracion" :categorie="peli.descripcionCorta" :director="peli.nombreDirector == null ? '' : peli.nombreDirector" :poster="peli.poster == null ? '' : peli.poster" />
         </div>
     </div>
 
@@ -23,20 +18,23 @@
 
 <script>
 import Pelicula from 'components/Pelicula.vue';
-import { reactive } from '@vue/runtime-core';
+import {
+    reactive
+} from '@vue/runtime-core';
 
 export default {
     name: 'Catalogo',
     components: {
         Pelicula
     },
-    props:{
+    props: {
         pelisFiltradas: Array
     },
     setup() {
         document.title = 'CheckFlix';
 
         const state = reactive({
+            current: 1
         });
 
         return {
@@ -53,40 +51,43 @@ export default {
     padding: 0;
 }
 
-.peliculas-container {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    gap: 0.5rem;
-    padding: 1rem;
-    padding-top: 0.7rem;
+.titulo-wrapper {
+    margin: 0 3rem;
+    margin-top: 0.7rem;
 
-    .titulo-wrapper {
-        grid-column: 1/-1;
-        margin-top: 0.7rem;
-        margin: 0 1rem;
+    .titulo {
+        font-size: 2rem;
+        font-weight: bold;
 
-        .titulo {
-            font-size: 2rem;
-            font-weight: bold;
-
-            .marca {
-                font-style: italic;
-
-                span {
-                    color: #E9C46A;
-                }
-            }
-
-        }
-
-        .subtitulo {
-            font-size: 1.4rem;
+        .marca {
             font-style: italic;
 
             span {
-                font-weight: bold;
+                color: #E9C46A;
             }
         }
+
     }
+
+    .subtitulo {
+        font-size: 1.4rem;
+        font-style: italic;
+
+        span {
+            font-weight: bold;
+        }
+    }
+
+    .pagination {
+        margin-top: 1rem;
+        display: grid;
+        place-items: center;
+    }
+}
+
+.peliculas-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    padding: 0.7rem;
 }
 </style>
